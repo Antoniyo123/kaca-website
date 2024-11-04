e8bd055aec757c03e9f7e06eb807a40d67e99639
Riswanton Ramudin <riswantonr2015@gmail.com>
2024-11-04 23:21:13 +0700

    change deskripsi project

--- src/component/DeskripsiProject.js
+++ src/component/DeskripsiProject.js
@@ -6,17 +6,16 @@ import arrowDownIcon from '../img/svg-assets/arrow-down.svg';
 const DeskripsiProject = () => {
   const allProjects = [
     { id: 1, title: "VIVO", image: require('../img/projects/aqsavivo.png'), brand: require('../img/brandimg/vivo.png') },
-    { id: 2, title: "Lazada", image: require('../img/projectdetail/project2.png'), brand: require('../img/brandimg/lazada.png')  },
-    { id: 3, title: "Netflix", image: require('../img/projectdetail/project3.png'), brand: require('../img/brandimg/netflix.png') },
+    { id: 2, title: "Fibemini", image: require('../img/projectdetail/project2.png'), brand: require('../img/brandimg/fibemini.png')  },
+    { id: 3, title: "LAZADA", image: require('../img/projectdetail/project3.png'), brand: require('../img/brandimg/lazada.png') },
     { id: 4, title: "Johnie Walker", image: require('../img/projectdetail/project4.png'), brand: require('../img/brandimg/jw.png')  },
     { id: 5, title: "SteveMadden", image: require('../img/projectdetail/project1.png'), brand: require('../img/brandimg/stevemadden.png')  },
     { id: 6, title: "Puma", image: require('../img/projectdetail/project6.png'), brand: require('../img/brandimg/puma.png')  },
-    { id: 7, title: "Disney", image: require('../img/projectdetail/project1.png'), brand: require('../img/brandimg/disney.png')  },
+    { id: 7, title: "Mitsubishi", image: require('../img/projectdetail/project1.png'), brand: require('../img/brandimg/disney.png')  },
     { id: 8, title: "LV", image: require('../img/projectdetail/project2.png'), brand: require('../img/brandimg/lv.png')  },
     { id: 9, title: "Samsung", image: require('../img/projectdetail/project3.png'), brand: require('../img/brandimg/samsung.png')  },
-    { id: 9, title: "Grab", image: require('../img/projectdetail/project3.png'), brand: require('../img/brandimg/grab.png')  },
-    { id: 9, title: "Dior", image: require('../img/projectdetail/project3.png'), brand: require('../img/brandimg/dior.png')  },
-    { id: 9, title: "Loreal", image: require('../img/projectdetail/project3.png'), brand: require('../img/brandimg/loreal.png')  },
+    { id: 10, title: "DIOR", image: require('../img/projectdetail/project3.png'), brand: require('../img/brandimg/dior copy.png')  },
+
     // { id: 4, title: "Project 10", image: require('../img/projectdetail/project4.png'), description: "Description for Project 4" },
     // { id: 5, title: "Project 11", image: require('../img/projectdetail/project1.png'), description: "Description for Project 5" },
     // { id: 6, title: "Project 12", image: require('../img/projectdetail/project6.png'), description: "Description for Project 6" },
@@ -25,7 +24,7 @@ const DeskripsiProject = () => {
   const [visibleProjects, setVisibleProjects] = useState(6);
 
   const loadMoreProjects = () => {
-    setVisibleProjects(prevVisible => Math.min(prevVisible + 6, allProjects.length));
+    setVisibleProjects(prevVisible => Math.min(prevVisible + 12, allProjects.length));
   };
 
   return (
